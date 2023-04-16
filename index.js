98669d5556e1b6419cfd997536ae0c4f7310a5d4
AliJoammis <114506096+AliJoammis@users.noreply.github.com>
2023-04-16 03:18:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,18 +10,16 @@ document.getElementById("p2").innerHTML = txt;
 
 var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomDiceImage = "dice"+randomNumber1+".png";
-var randomImageSource = "images/"+randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src",randomImageSource);
+image1.setAttribute("src",randomDiceImage);
 
 
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 var randomDiceImage = "dice"+randomNumber2+".png";
-var randomImageSource = "images/"+randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[1];
-image1.setAttribute("src",randomImageSource);
+image1.setAttribute("src",randomDiceImage);
 
 if(randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML = "Player 1 wins 🚩!";
